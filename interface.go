0c273772c0744e3b585edf3e8f75fae5addf09d9
mike andrews <mra@xoba.com>
2019-11-26 06:32:07 -0500

    more comments on interface

--- interface.go
+++ interface.go
@@ -8,8 +8,8 @@ type StorageCombinator interface {
 	Find(string) (Reference, error) // a sort of query or naming facility
 	Get(Reference) (interface{}, error)
 	Put(Reference, interface{}) error
-	Merge(Reference, interface{}) error
 	Delete(Reference) error
+	Merge(Reference, interface{}) error // somehow update the reference
 }
 
 type Reference interface {
