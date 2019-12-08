62c002e4ba4388414759d66b80623f0718b0e210
mike andrews <mra@xoba.com>
2019-12-08 05:42:40 -0500

    return any api body even on error

--- api.go
+++ api.go
@@ -1,6 +1,9 @@
 package sc
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -20,6 +23,14 @@ func (a APICombinator) Get(r Reference) (interface{}, error) {
 	resp, err := a.e.Get(r)
 	if err != nil {
 		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		defer resp.Body.Close()
+		w := new(bytes.Buffer)
+		if _, err := io.Copy(w, resp.Body); err != nil {
+			return nil, err
+		}
+		return w.Bytes(), fmt.Errorf("bad status: %q", resp.Status)
 	}
 	return resp.Body, nil
 }
