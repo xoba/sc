563e7534f02bd028cb9cffbab8d828cce43c426e
mike andrews <mra@xoba.com>
2019-12-08 09:30:26 -0500

    better caching strategy

--- cache.go
+++ cache.go
@@ -8,28 +8,47 @@ func NewCache(underlying, cache StorageCombinator) *Cache {
 }
 
 type Cache struct {
-	u, c StorageCombinator
+	u, c, tmp StorageCombinator
 }
 
 func (self Cache) Get(r Reference) (interface{}, error) {
 	if i, err := self.c.Get(r); err == nil {
 		return i, err
 	}
-	return self.u.Get(r)
+	if i, err := self.u.Get(r); err != nil {
+		return nil, err
+	} else if err := self.c.Put(r, i); err != nil {
+		return nil, err
+	}
+	return self.Get(r)
 }
 
 func (self Cache) Put(r Reference, i interface{}) error {
-	if err := self.c.Put(r, i); err != nil {
+	if err := self.c.Delete(r); err != nil {
 		return err
 	}
-	return self.u.Put(r, i)
+	if err := self.u.Put(r, i); err != nil {
+		return err
+	}
+	tmpCopy, err := self.u.Get(r)
+	if err != nil {
+		return err
+	}
+	return self.c.Put(r, tmpCopy)
 }
 
 func (self Cache) Merge(r Reference, i interface{}) error {
-	if err := self.c.Merge(r, i); err != nil {
+	if err := self.c.Delete(r); err != nil {
 		return err
 	}
-	return self.u.Merge(r, i)
+	if err := self.u.Merge(r, i); err != nil {
+		return err
+	}
+	tmpCopy, err := self.u.Get(r)
+	if err != nil {
+		return err
+	}
+	return self.c.Put(r, tmpCopy)
 }
 
 func (self Cache) Delete(r Reference) error {
