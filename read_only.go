db2c2e9a905d95c6cffaf066c3ace05d27ce67ec
mike andrews <mra@xoba.com>
2019-12-09 09:39:14 -0500

    a read-only combinator

---
+++ read_only.go
@@ -0,0 +1,31 @@
+package sc
+
+import "errors"
+
+type ReadOnly struct {
+	c StorageCombinator
+}
+
+func NewReadOnly(c StorageCombinator) *ReadOnly {
+	return &ReadOnly{
+		c: c,
+	}
+}
+
+var ReadOnlyError = errors.New("read only")
+
+func (ro ReadOnly) Get(r Reference) (interface{}, error) {
+	return ro.c.Get(r)
+}
+
+func (ro ReadOnly) Put(Reference, interface{}) error {
+	return ReadOnlyError
+}
+
+func (ro ReadOnly) Delete(Reference) error {
+	return ReadOnlyError
+}
+
+func (ro ReadOnly) Merge(Reference, interface{}) error {
+	return ReadOnlyError
+}
