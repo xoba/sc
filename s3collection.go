1d32795a9d668b031588b23ebca0cb2eff4c39a6
mike andrews <mra@xoba.com>
2020-04-07 08:44:25 -0400

    oops, need a termination for recursion

--- s3collection.go
+++ s3collection.go
@@ -197,6 +197,9 @@ func (c S3Collection) store(recs ...S3Record) error {
 
 // divides a list into sub-lists of maximal length
 func divide(list []string, max int) (out [][]string) {
+	if len(list) == 0 {
+		return
+	}
 	add := func(lists [][]string) {
 		for _, x := range lists {
 			if len(x) == 0 {
