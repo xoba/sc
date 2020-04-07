c4bb5e8a63c7ea7aa1358fb59429f6b4eceb9493
mike andrews <mra@xoba.com>
2020-04-07 08:42:50 -0400

    fixing a dumb bug

--- s3collection.go
+++ s3collection.go
@@ -197,9 +197,6 @@ func (c S3Collection) store(recs ...S3Record) error {
 
 // divides a list into sub-lists of maximal length
 func divide(list []string, max int) (out [][]string) {
-	if len(list) < max {
-		return
-	}
 	add := func(lists [][]string) {
 		for _, x := range lists {
 			if len(x) == 0 {
